a3cd779a3355e71d3891da692d5ffad1ebf9b281
jidelaguila <jimena.delaguila2@gmail.com>
2019-10-14 12:30:49 +0200

    probando promesa

--- main.mjs
+++ main.mjs
@@ -8,48 +8,84 @@ let direcc;
 let arrayResultado = [{}];
 let arrayTemp = [];
 
+const PROMESA = new Promise((resolve, rejected)=>{
+    
+    let arrayFicheros = [];
+    let arrayDirectorios = []; 
 
-
-function lectura(){
-let test
-let ruta ="./"
-let array = []
-    test = fs.readdirSync("./");
-    // console.log(test)
-    // test.forEach(element => {console.log(element)})
+let recursiva = (ruta = "./")=>{
+   fs.readdirSync(ruta).forEach(element => {   
+    if (fs.statSync(element).isDirectory() === false){      
+    arrayFicheros.push(ruta+element)       
+    }    
+    
+    if (fs.statSync(element).isDirectory() === true && element != "node_modules"){
         
-    let recursiva = (ruta)=>{
+        rejected(arrayDirectorios.push("/"+element))
 
-        fs.readdirSync(ruta).forEach(element => {
-                
-        if (fs.statSync(element).isDirectory() === false){
-            array.push(ruta+element)
-        }
-
-        ()=>{
-             if (fs.statSync(element).isDirectory() === true && element != "node_modules"){
-                (param = "./")=>{
-                    ruta = param + element
-                }
-                recursiva ()
-                // ruta = "./" + element + "/";
-            
-                // console.log("if TRUE RUTA:" + "./" + element + "/")
-                // console.log("if true valor archivo: " + fs.readdirSync(ruta+element))
-        }
-        }
-       
-    })      
-    ;
-    }
-    recursiva(ruta)
-
-console.log(array)
-
+        // ruta = "./" + element + "/";
+    
+        // console.log("if TRUE RUTA:" + "./" + element + "/")
+        // console.log("if true valor archivo: " + fs.readdirSync(ruta+element))
+    }      
+}); 
 }
 
+recursiva()    
+resolve(arrayFicheros)/* array.push */
 
-lectura();
+
+})
+.then((e)=>console.log(e),
+(i)=>console.log(i))
+
+// .then((ficheros) =>{
+//     console.log(ficheros),
+//     (directorios) =>{
+//         console.log(directorios)}
+// })
+
+
+// function lectura(){
+// let test
+// let ruta ="./"
+// let array = []
+//     test = fs.readdirSync("./");
+//     // console.log(test)
+//     // test.forEach(element => {console.log(element)})
+        
+//     let recursiva = (ruta)=>{
+
+//         fs.readdirSync(ruta).forEach(element => {
+                
+//         if (fs.statSync(element).isDirectory() === false){
+//             array.push(ruta+element)
+//         }
+
+//         ()=>{
+//              if (fs.statSync(element).isDirectory() === true && element != "node_modules"){
+//                 (param = "./")=>{
+//                     ruta = param + element
+//                 }
+//                 recursiva ()
+//                 // ruta = "./" + element + "/";
+            
+//                 // console.log("if TRUE RUTA:" + "./" + element + "/")
+//                 // console.log("if true valor archivo: " + fs.readdirSync(ruta+element))
+//         }
+//         }
+       
+//     })      
+//     ;
+//     }
+//     recursiva(ruta)
+
+// console.log(array)
+
+// }
+
+
+// lectura();
 
 //     let ruta;
 //     direcc = fs.readdirSync(ruta);
